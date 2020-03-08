a4300ed840372cc314fc723677f84ff101b938ce
Julian Baños y Rivas <julian.b@sirena.app>
2020-03-08 13:25:23 -0300

    modify user does not support user change of account

--- src/controllers/users/modify.js
+++ src/controllers/users/modify.js
@@ -3,22 +3,26 @@
 const User = require('../../models/user')
 
 const modifyUser = async (ctx) => {
-  const userToModify = await User.findById(ctx.params.id).catch((error) => {
-    ctx.status = 500
-    ctx.body = { error: error, status: 'failed' }
-  })
-  if (userToModify) {
-    await User.updateOne({ _id: ctx.params.id }, ctx.request.body.user, { runValidators: true })
-      .then(() => {
-        ctx.status = 201
-        ctx.body = { status: 'success' }
-      })
-      .catch((error) => {
-        ctx.status = 400
-        ctx.body = { error: error, status: 'failed' }
-      })
+  if (ctx.request.body.user.account) {
+    ctx.throw(400, 'Cannot change user account')
   } else {
-    ctx.throw(404, 'User not found')
+    const userToModify = await User.findById(ctx.params.id).catch((error) => {
+      ctx.status = 500
+      ctx.body = { error: error, status: 'failed' }
+    })
+    if (userToModify) {
+      await User.updateOne({ _id: ctx.params.id }, ctx.request.body.user, { runValidators: true })
+        .then(() => {
+          ctx.status = 201
+          ctx.body = { status: 'success' }
+        })
+        .catch((error) => {
+          ctx.status = 400
+          ctx.body = { error: error, status: 'failed' }
+        })
+    } else {
+      ctx.throw(404, 'User not found')
+    }
   }
 }
 
